c219eca9bb727c6cf156c426197e4cddaeea9dde
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-12-29 00:04:10 +0100

    Add simulation of planets moving

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ pygame.init()
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Solar System Simulation")
 
-def main():
-    run_simulation = True 
-    
-    sun = Planet(SUN_MASS, SUN_RADIUS / 50, 0, 0, SUN_COLOR)
+FONT = pygame.font.SysFont("comicsans", 16)
+
+def initialize_planets():
+    sun = Planet(SUN_MASS, SUN_RADIUS / 50, 0, 0, SUN_COLOR, True)
     mercury = Planet(MERCURY_MASS, MERCURY_RADIUS, MERCURY_DISTANCE, 0, MERCURY_COLOR)
     venus = Planet(VENUS_MASS, VENUS_RADIUS, VENUS_DISTANCE, 0, VENUS_COLOR)
     earth = Planet(EARTH_MASS, EARTH_RADIUS, EARTH_DISTANCE, 0, EARTH_COLOR)
@@ -21,19 +21,37 @@ def main():
     uranus = Planet(URANUS_MASS, URANUS_RADIUS, URANUS_DISTANCE, 0, URANUS_COLOR)
     neptun = Planet(NEPTUN_MASS, NEPTUN_RADIUS, NEPTUN_DISTANCE, 0, NEPTUN_COLOR)
     
-    planets = [sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptun]
+    mercury.v_y = MERCURY_VELOCITY
+    venus.v_y = VENUS_VELOCITY
+    earth.v_y = EARTH_VELOCITY
+    mars.v_y = MARS_VELOCITY
+    jupiter.v_y = JUPITER_VELOCITY
+    saturn.v_y = SATURN_VELOCITY
+    uranus.v_y = URANUS_VELOCITY
+    neptun.v_y = NEPTUN_VELOCITY
+
+    return [sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptun]
+
+def main():
+    run_simulation = True
+    clock = pygame.time.Clock() 
+    
+    planets = initialize_planets()
     
     while run_simulation:
+        clock.tick(60)
+        WINDOW.fill((0, 0, 0))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run_simulation = False
-        
-        WINDOW.fill((0, 0, 0))
-        
+                
         for planet in planets:
-            planet.draw(WINDOW)
+            planet.update_position(planets)
+            planet.draw(WINDOW, FONT)
         
         pygame.display.update()
+
     pygame.quit()
 
 if __name__ == "__main__":
